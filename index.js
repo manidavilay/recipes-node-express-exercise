de1a19fcae60fece789e7b22609075a856edccfb
Manida Vilay <vilaymanida@gmail.com>
2024-11-26 16:27:56 +0100

    create recipe api

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ app.get("/api/recipes", async (req, res) => {
     }
 })
 
+// Create recipe API
+app.post("/api/recipes", async (req, res) => {
+    try {
+        const recipe = await Recipe.create(req.body)
+        res.status(200).json(recipe)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
 mongoose.connect(DBConnectionString)
 .then(() => {
     console.log("Connected to database !")
