4830836f6bf03780e6e134a3c580695983638790
Manida Vilay <vilaymanida@gmail.com>
2024-11-26 16:54:10 +0100

    get recipe by id api

--- index.js
+++ index.js
@@ -23,6 +23,23 @@ app.listen(port, () => {
 app.get("/api/recipes", async (req, res) => {
     try {
         const recipe = await Recipe.find({})
+        if (!recipe) {
+            res.status(404).json({ message: "There are no recipes yet !" })
+        }
+        res.status(200).json(recipe)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+// Get recipe by id API
+app.get("/api/recipes/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const recipe = await Recipe.findById(id)
+        if (!recipe) {
+            res.status(404).json({ message: "The recipe you are looking for has not been found !" })
+        }
         res.status(200).json(recipe)
     } catch (error) {
         res.status(500).json({ message: error.message })
