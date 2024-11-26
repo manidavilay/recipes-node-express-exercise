691761c78891cc79e723dd34c553844584ef3c02
Manida Vilay <vilaymanida@gmail.com>
2024-11-26 15:48:28 +0100

    install mongodb mongoose, connect to db

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
 const app = express()
+
+const DBConnectionString = process.env.MONGODB_URI
 
 app.get("/", (req, res) => {
     res.send("This is from node API")
@@ -7,4 +12,11 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
+})
+
+mongoose.connect(DBConnectionString)
+.then(() => {
+    console.log("Connected to database !")
+}).catch(() => {
+    console.log("Connection failed")
 })
