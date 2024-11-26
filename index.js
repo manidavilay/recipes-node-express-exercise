44239b877d557be7f3c91c237446f6ec12896f50
Manida Vilay <vilaymanida@gmail.com>
2024-11-26 16:27:21 +0100

    create get all recipes api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const Recipe = require("./models/recipe.model")
 require("dotenv").config()
 
 const app = express()
+
+// Middleware
+app.use(express.json())
 
 const port = process.env.PORT || 3000
 const DBConnectionString = process.env.MONGODB_URI
@@ -13,6 +17,16 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
     console.log("Server is running on port 3000")
+})
+
+// Get all recipes API
+app.get("/api/recipes", async (req, res) => {
+    try {
+        const recipe = await Recipe.find({})
+        res.status(200).json(recipe)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
 mongoose.connect(DBConnectionString)
